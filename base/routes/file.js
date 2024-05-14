c8d70a385e4f272b4999481a012fce663914ba76
zhangshuang <zhangshuang@forensix.cn>
2024-05-14 22:23:57 +0800

    fix

--- base/routes/file.js
+++ base/routes/file.js
@@ -1,16 +1,47 @@
 const router = require("express").Router();
-const formidable = require("formidable");
-console.log(formidable)
-router.post("/upload", (req, res, next) => {
-  const form = formidable({})
-  console.log(form)
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return next(err)
+const multer = require("multer");
+const path = require("path")
+const fs = require("fs")
+const { promisify } = require("util")
+
+// const upload = multer({ dest: "uploads/" })
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/")
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + file.originalname)
     }
   })
-  console.log(files, fields)
-  res.json({files, fields});
+})
+
+// 清空上传目录
+const readdir = promisify(fs.readdir)
+const unlink = promisify(fs.unlink)
+
+const emptyDirectory = async (req, res, next) => {
+  try {
+    const directory = path.join(__dirname, "../uploads")
+    const files = await readdir(directory)
+    for (const file of files) {
+      await unlink(path.join(directory, file))
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+// console.log(formidable)
+router.post("/upload", emptyDirectory, upload.single('file'), (req, res) => {
+  console.log('***************')
+  // console.log(form)
+  // res.send("upload success");
+  res.json({
+    files: req.file,
+    data: req.body
+  })
 });
 
 module.exports = router;
